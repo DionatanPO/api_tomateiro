e48dc6c05e675454d43f863dd0b04dcb68592696
Dionatan <dionatanoliveirapc@gmail.com>
2020-12-19 19:04:06 -0300

    Dev api 1.0

---
+++ src/main/java/com/example/api_tomateiro/ApiTomateiroApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_tomateiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTomateiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTomateiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/api_tomateiro/ApiTomateiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_tomateiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTomateiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
